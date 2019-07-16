15253afa6aef6c2d8a1dd2fa8d65bcd3a2c64591
Saumya Pandey <Sp160899@gmail.com>
2019-07-16 15:07:30 +0530

    Permutations

--- CODINGBLOCKS/Recursion and Backtracking/Permutations/main.cpp
+++ CODINGBLOCKS/Recursion and Backtracking/Permutations/main.cpp
@@ -1,9 +1,28 @@
-#include <iostream>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
-    cout << "Hello world!" << endl;
-    return 0;
+void permutation(char *input, char *output,int i, int j){
+     if(input[i]=='\0'){
+         cout<<output<<" ";
+         return;
+     }
+
+     output[j]=input[i];
+     permutation(input+1,output,0,j+1);
+
+     for(int k=0;k<strlen(input);k++){
+         if(i!=k){
+             output[j]=input[k];
+             swap(input[0],input[k]);
+             permutation(input+1,output,0,j+1);
+             swap(input[0],input[k]);
+         }
+     }
+}
+int main() {
+   char input[100];
+   char output[100];
+   cin>>input;
+   permutation(input,output,0,0);
+   return 0;
 }
