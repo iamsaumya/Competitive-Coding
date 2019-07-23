4ed18f9aec806408b17b687865a17ff42a21b76b
Saumya Pandey <Sp160899@gmail.com>
2019-07-23 20:36:48 +0530

    Subset sum easy **Unsolved**

---
+++ CODINGBLOCKS/Recursion and Backtracking/Subset sum easy/main.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+bool printYES = false;
+void subset(ll arr[],ll n,ll pos, vector<ll>v){
+    if(pos==n && printYES==false){
+      if(v.size()>0){
+          ll sum=0;
+          for(ll i=0;i<v.size();i++)
+               sum += v[i];
+         if(sum==0){
+            cout<<"Yes"<<endl;
+            printYES = true;
+         }
+     }
+     return;
+    }
+    v.push_back(arr[pos]);
+    subset(arr,n,pos+1,v);
+    v.pop_back();
+    subset(arr,n,pos+1,v);
+}
+int main()
+{
+    ll t,n;
+    ll arr[4];
+    cin>>t;
+    while(t--){
+        cin>>n;
+        for(ll i=0;i<n;i++){
+            cin>>arr[i];
+        }
+        vector<ll>v;
+        subset(arr,n,0,v);
+        if(!printYES)
+            cout<<"No"<<endl;
+        v.clear();
+    }
+    return 0;
+}
