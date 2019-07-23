8467c40bb20ec7badaca07cfdb7a2fd8c83bbb51
Saumya Pandey <Sp160899@gmail.com>
2019-07-23 21:54:50 +0530

    Update Subset sum easy

--- CODINGBLOCKS/Recursion and Backtracking/Subset sum easy/main.cpp
+++ CODINGBLOCKS/Recursion and Backtracking/Subset sum easy/main.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 #define ll long long int
 bool printYES = false;
+
 void subset(ll arr[],ll n,ll pos, vector<ll>v){
     if(pos==n && printYES==false){
       if(v.size()>0){
@@ -23,14 +24,14 @@ void subset(ll arr[],ll n,ll pos, vector<ll>v){
 int main()
 {
     ll t,n;
-    ll arr[4];
+    ll arr[10];
     cin>>t;
+    vector<ll>v;
     while(t--){
         cin>>n;
         for(ll i=0;i<n;i++){
             cin>>arr[i];
         }
-        vector<ll>v;
         subset(arr,n,0,v);
         if(!printYES)
             cout<<"No"<<endl;
