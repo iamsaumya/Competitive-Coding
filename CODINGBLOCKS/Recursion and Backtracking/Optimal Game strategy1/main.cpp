c62d7f8e90cbb19f939ab29cc0fc8df216f93887
Saumya Pandey <Sp160899@gmail.com>
2019-07-24 19:20:49 +0530

    Solve Optimal Game strategy1

--- CODINGBLOCKS/Recursion and Backtracking/Optimal Game strategy1/main.cpp
+++ CODINGBLOCKS/Recursion and Backtracking/Optimal Game strategy1/main.cpp
@@ -1,3 +1,35 @@
+// Jakas approach
+
+#include<bits/stdc++.h>
+using namespace std;
+int maxval = INT_MIN;
+
+void optimalGamestrategy(int arr[],int size, int start, int end,int sum){
+        if(size<=0){
+            maxval = max(maxval,sum);
+            return;
+        }
+
+        sum += arr[start];
+        optimalGamestrategy(arr,size-3,start+2,end-1,sum);
+        sum-= arr[start];
+
+        sum+=arr[end];
+        optimalGamestrategy(arr,size-3,start+1,end-2,sum);
+        sum -= arr[end];
+}
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++)
+        cin>>arr[i];
+    optimalGamestrategy(arr,n,0,n-1,0);
+    cout<<maxval<<endl;
+}
+
+/* Bakwaas Approach
+
 #include<bits/stdc++.h>
 using namespace std;
 #define ll long long int
@@ -41,3 +73,5 @@ int main() {
 	cout<<piyush<<endl;
 	return 0;
 }
+
+*/
