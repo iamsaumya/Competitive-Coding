53a1334b4bc2dc2eaf0ccf35d1cec6b1b4f137a5
Saumya Pandey <Sp160899@gmail.com>
2019-08-14 15:30:15 +0530

    Solve Matrix Queries
    Declare vector and array as public

--- CODINGBLOCKS/Segment Trees/Matrix Queries/main.cpp
+++ CODINGBLOCKS/Segment Trees/Matrix Queries/main.cpp
@@ -5,6 +5,8 @@ using namespace std;
 #define v2d vector<vector<ll>>
 #define v1d vector<ll>
 int r,n,q;
+int matrix[100005][3][3];
+v3d tree(400020,v2d(3,v1d(3)));
 
 v2d multiply(v2d &a,v2d &b){ // Make a multiply Function.
     v2d c(3,v1d(3));
@@ -21,7 +23,7 @@ v2d multiply(v2d &a,v2d &b){ // Make a multiply Function.
 	  return c;
 }
 
-void buildtree(int matrix[][3][3], int ss, int se, v3d &tree, int index){
+void buildtree(int ss, int se,int index){
     if(ss==se){
          for(int j=1;j<=2;j++){
 	        for(int k=1;k<=2;k++){
@@ -32,14 +34,14 @@ void buildtree(int matrix[][3][3], int ss, int se, v3d &tree, int index){
     }
 
     int mid = ss + (se-ss)/2;
-    buildtree(matrix,ss,mid,tree,2*index);
-    buildtree(matrix,mid+1,se,tree,2*index+1);
+    buildtree(ss,mid,2*index);
+    buildtree(mid+1,se,2*index+1);
 
     tree[index]=multiply(tree[2*index],tree[2*index+1]);
     return;
 }
 
-v2d query(v3d tree, ll ss, ll se, ll l, ll r, ll index){
+v2d query(ll ss, ll se, ll l, ll r, ll index){
   if(l>se || r<ss){
       v2d identity(3,v1d(3)); // declare 2d vector like vector<vector<int>>v(3,vector<int>)
       return identity;
@@ -55,14 +57,14 @@ v2d query(v3d tree, ll ss, ll se, ll l, ll r, ll index){
   v2d mat1;
 
   if(r<=mid){
-    return query(tree,ss,mid,l,r,2*index);
+    return query(ss,mid,l,r,2*index);
   }
   if(l>mid){
-    return query(tree,mid+1,se,l,r,2*index+1);
+    return query(mid+1,se,l,r,2*index+1);
   }
 
-  mat = query(tree,ss,mid,l,r,2*index);
-  mat1 = query(tree,mid+1,se,l,r,2*index+1);
+  mat = query(ss,mid,l,r,2*index);
+  mat1 = query(mid+1,se,l,r,2*index+1);
 
   return multiply(mat,mat1);
 
@@ -70,7 +72,7 @@ v2d query(v3d tree, ll ss, ll se, ll l, ll r, ll index){
 int main() {
 
     cin>>r>>n>>q;
-    int matrix[n+1][3][3];
+
     for(int i=1;i<=n;i++){
         for(int j=1;j<=2;j++){
             for(int k=1;k<=2;k++)
@@ -78,13 +80,13 @@ int main() {
         }
     }
 
-    v3d tree(4*n+1, v2d(3,v1d(3)));
-    buildtree(matrix,1,n,tree,1);
+
+    buildtree(1,n,1);
     v2d temp;
     int x,y;
     while(q--){
         cin>>x>>y;
-        temp = query(tree,1,n,x,y,1);
+        temp = query(1,n,x,y,1);
         for(int i=1;i<=2;i++){
           for(int j=1;j<=2;j++)
                 cout<<temp[i][j]<<" ";
