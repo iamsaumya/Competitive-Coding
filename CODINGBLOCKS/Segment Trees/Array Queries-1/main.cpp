0f9a2b7e56c876fda939c3e542f73b388ae3f1a9
Saumya Pandey <Sp160899@gmail.com>
2019-08-18 17:36:27 +0530

    Solve Array Queries
    1. Not need to buildtree
    As the value of a range of node depends on nodes which are not there in the range.
    Ex: range 0-2 node's value also depend upon node 4,3 etc.
    So we will keep adding the arr[i] in the ranges it is required. Also use Lazy propogation.

--- CODINGBLOCKS/Segment Trees/Array Queries-1/main.cpp
+++ CODINGBLOCKS/Segment Trees/Array Queries-1/main.cpp
@@ -6,30 +6,21 @@ ll n;
 ll k;
 ll q;
 ll T,L,R;
-vector<int>arr(100001);
-vector<ll>vec(400020);
-
-void buildtree(ll ss, ll se, ll index){
-    if(ss==se){
-        int sizeofarr = n;
-        sizeofarr += ss;
-        ll sum = 0;
-        for(int i=1;i<=k;i++){
-            int ind = (sizeofarr - i ) % n;
-            sum += arr[ind];
-        }
-        vec[index] = sum;
-        return;
-    }
-
-    ll mid = ss + (se-ss)/2;
-    buildtree(ss,mid,2*index);
-    buildtree(mid+1,se,2*index+1);
-    vec[index] = vec[2*index] + vec[2*index+1];
-    return;
-}
+int arr[100020];
+ll vec[400100];
+ll lazy[400100]={0};
 
 ll query(ll ss, ll se , ll l, ll r,ll index){
+
+    if(lazy[index]){
+        vec[index] += (se-ss+1)*lazy[index];
+        if(ss!=se){
+            lazy[2*index] += lazy[index];
+            lazy[2*index+1] += lazy[index];
+        }
+        lazy[index] = 0;
+    }
+
     if(ss>r || se<l){
         return 0;
     }
@@ -39,17 +30,8 @@ ll query(ll ss, ll se , ll l, ll r,ll index){
     }
 
     ll mid = ss + (se-ss)/2;
-
     ll left;
     ll right;
-
-    if(r<=mid){
-        return query(ss,mid,l,r,2*index);
-    }
-
-    if(l>=mid+1){
-        return query(mid+1,se,l,r,2*index+1);
-    }
 
     left = query(ss,mid,l,r,2*index);
     right = query(mid+1,se,l,r,2*index+1);
@@ -57,49 +39,73 @@ ll query(ll ss, ll se , ll l, ll r,ll index){
     return left + right;
 }
 
-void update(ll ss, ll se, ll index, ll node, ll val){
-    if(node<ss && node>se){
+void update(ll ss, ll se,ll l, ll r, ll index, ll val){
+
+    if(lazy[index]){
+        vec[index] += (se-ss+1)*lazy[index];
+        if(ss!=se){
+            lazy[2*index] += lazy[index];
+            lazy[2*index+1] += lazy[index];
+        }
+        lazy[index] = 0;
+    }
+
+    if(ss>r || se<l){
         return;
     }
 
-    if(ss==se){
-        vec[index] -= arr[L-1];
-        vec[index] += val;
+    if(ss>=l && se<=r){
+        vec[index] += (se-ss+1)*val;
+        if(ss!=se){
+            lazy[2*index] += val;
+            lazy[2*index+1] += val;
+        }
         return;
     }
 
     ll mid = ss+(se-ss)/2;
 
-    update(ss,mid,2*index,node,val);
-    update(mid+1,se,2*index+1,node,val);
+    update(ss,mid,l,r,2*index,val);
+    update(mid+1,se,l,r,2*index+1,val);
     vec[index] = vec[2*index]+vec[2*index+1];
     return;
 }
 
 
 int main()
-{   cin>>n>>k>>q;
-    for(int i=0;i<n;i++) cin>>arr[i];
-    buildtree(0,n-1,1);
+{
+    ios_base::sync_with_stdio(0);
+    cin>>n>>k>>q;
+    for(int i=0;i<n;i++) {
+     cin>>arr[i];
+     ll l = i, r = i+k-1;
+     if(r>=n){
+        update(0,n-1,l,n-1,1,arr[i]);
+        update(0,n-1,0,r-n,1,arr[i]);
+     }
+     else{
+        update(0,n-1,l,r,1,arr[i]);
+     }
+    }
+
     while(q--){
 
         cin>>T>>L>>R;
         if(T==2){
-            cout<< query(0,n-1,L-1,R-1,1)<<endl;
-
+            ll ans = query(0,n-1,L-1,R-1,1);
+            cout<<ans<<endl;
         }
         else{
-            ll lower = L-1;
-            for(int i=0;i<k;i++){
-               update(0,n-1,1,lower,R);
-               lower = (lower+1)%n;
+           ll l = L-1;
+           ll r = L+ k-2;
+           if(r>=n){
+                update(0,n-1,l,n-1,1,R-arr[l]);
+                update(0,n-1,0,r-n,1,R-arr[l]);
             }
-
-            arr[L-1] =  R;
-             for(int i=0;i<n;i++)
-                cout<<arr[i]<<" ";
-
-            cout<<endl;
+          else{
+                update(0,n-1,l,r,1,R-arr[l]);
+            }
+            arr[l] = R;
         }
     }
     return 0;
