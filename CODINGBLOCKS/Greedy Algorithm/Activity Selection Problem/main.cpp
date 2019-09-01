529b931ca686b08108c1f4f9fd9fc711f69ad033
Saumya Pandey <Sp160899@gmail.com>
2019-09-02 00:11:37 +0530

    Solve Maximum Circles

--- CODINGBLOCKS/Greedy Algorithm/Activity Selection Problem/main.cpp
+++ CODINGBLOCKS/Greedy Algorithm/Activity Selection Problem/main.cpp
@@ -2,31 +2,44 @@
 #include<vector>
 #include<algorithm>
 using namespace std;
+
+bool cmp(pair<int,int> a,pair<int,int> b)
+{
+    return(b.second>a.second);
+}
+
+#define ll long long int
 int main() {
+    ll n;
+    cin>>n;
+    vector<pair<ll,ll>>vec;
+    for(ll i=0;i<n;i++){
+       ll a;
+       ll b;
+       cin>>a>>b;
+       vec.push_back({a,b});
+    }
 
-	int t;
-	cin>>t;
-	while(t--){
-		int n;
-		cin>>n;
-		vector<pair<int,int>>vec;
-		while(n--){
-               int a;
-			   int b;
-			   cin>>a>>b;
-			   vec.push_back({b,a});
-		}
-		sort(vec.begin(),vec.end());
+   sort(vec.begin(),vec.end());
 
-		int val = vec[0].first; // first activity which ends
-		int ans = 1;
-		for(int i=1;i<vec.size();i++){
-             if(vec[i].second>=val){
-				 ans++;
-				 val = vec[i].first;
-			 }
-		}
-		cout<<ans<<endl;
-	}
+    for(int i=0;i<n;i++){
+       int a = vec[i].first;
+       int b = vec[i].second;
+       ll f = a - b;
+       ll s = a + b;
+       vec[i]={f,s};
+    }
+
+       sort(vec.begin(),vec.end(),cmp);
+
+    int val =  vec[0].second;
+    int ans = 1;
+    for(ll i=1;i<n;i++){
+        if(vec[i].first>=val){
+            ans++;
+           val = vec[i].second;
+        }
+    }
+    cout<<n-ans<<endl;
 	return 0;
 }
