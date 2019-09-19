ac1d4b86233fb289e2493dde6a4ace7fe2c14958
Saumya Pandey <Sp160899@gmail.com>
2019-09-19 16:44:35 +0530

    Revert "Revert "Update DELISH to use Fast I/O""
    This reverts commit 3416df3f9c7377b9e787454b3d852e779f6f44d7.

--- CCDSAP/DYNAMIC PROGRAMMING/DELISH/main.cpp
+++ CCDSAP/DYNAMIC PROGRAMMING/DELISH/main.cpp
@@ -1,14 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long int
+ll read()
+{
+        ll n=0;
+        char c;int f=0;
+        //fast method to read character
+        c=getchar_unlocked();
+        while(c<'0'||c>'9')
+        {
 
+        if(c=='-')
+        f=1;
+        	c=getchar_unlocked();
+        }
+        while(c>='0'&&c<='9')
+        {
+        	n=n*10+c-'0';
+        	c=getchar_unlocked();
+        }
+        if(!f)
+        return n;
+        else
+        return -n;
+}
 int main()
 {
     int t;
-    cin>>t;
+    t = read();
     while(t--){
-        int n;
-        cin>>n;
+        ll n;
+        n = read();
         ll arr[10001];
         ll dplmax[10001];
         ll dplmin[10001];
@@ -16,7 +38,7 @@ int main()
         ll dprmin[10001];
 
         for(int i=0;i<n;i++) {
-            cin>>arr[i];
+            arr[i] = read();
             dplmax[i]=dplmin[i]=dprmax[i]=dprmin[i]=arr[i];
         }
 
